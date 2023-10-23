35597fb6b5d37cc3a0953ce54c6f53adb74b2c12
rdgronii <rgroner2@gmail.com>
2023-10-23 11:02:56 -0500

    amend previous commit

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var usedWords: [String] = []
+    @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
     
@@ -33,6 +33,7 @@ struct ContentView: View {
         }
         .navigationTitle(rootWord)
         .onSubmit(addNewWord)
+        .onAppear(perform: startGame)
     }
     
     func addNewWord() {
@@ -44,6 +45,18 @@ struct ContentView: View {
             usedWords.insert(answer, at: 0)
         }
         newWord = ""
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle.")
     }
 }
 
